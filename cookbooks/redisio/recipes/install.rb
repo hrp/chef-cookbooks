df5da7d548df64127a72051b39bc1cbe5687e14d
bwu <bwu@e488b3dd-d2c1-224d-8c88-7181da6ea77a>
2013-01-30 21:43:35 +0000

    redisio cookbook https://github.com/brianbianco/redisio
    git-svn-id: https://pf-subversion.hq.practicefusion.com/svn/pf.build/trunk/chef-repo@2106 e488b3dd-d2c1-224d-8c88-7181da6ea77a

---
+++ cookbooks/redisio/recipes/install.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook Name:: redisio
+# Recipe:: install
+#
+# Copyright 2012, Brian Bianco <brian.bianco@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+include_recipe 'redisio::default'
+
+redis = node['redisio']
+location = "#{redis['mirror']}/#{redis['base_name']}#{redis['version']}.#{redis['artifact_type']}"
+
+redisio_install "redis-servers" do
+  version redis['version']
+  download_url location
+  default_settings redis['default_settings']
+  servers redis['servers']
+  safe_install redis['safe_install']
+  base_piddir redis['base_piddir']
+end
+
+# Create a service resource for each redis instance, named for the port it runs on.
+redis['servers'].each do |current_server|
+  service "redis#{current_server['port']}" do
+    start_command "/etc/init.d/redis#{current_server['port']} start"
+    stop_command "/etc/init.d/redis#{current_server['port']} stop"
+    status_command "pgrep -lf 'redis.*#{current_server['port']}' | grep -v 'sh'"
+    restart_command "/etc/init.d/redis#{current_server['port']} stop && /etc/init.d/redis#{current_server['port']} start"
+    supports :start => true, :stop => true, :restart => true, :status => false
+  end
+end
+
