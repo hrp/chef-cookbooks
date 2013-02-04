d9575ca8c3960a67332b33d79bdee01dd487a0dd
bwu <bwu@e488b3dd-d2c1-224d-8c88-7181da6ea77a>
2013-02-04 22:37:16 +0000

    default attributes modified.
    git-svn-id: https://pf-subversion.hq.practicefusion.com/svn/pf.build/trunk/chef-repo@2148 e488b3dd-d2c1-224d-8c88-7181da6ea77a

--- cookbooks/splunk-forwarder/attributes/default.rb
+++ cookbooks/splunk-forwarder/attributes/default.rb
@@ -4,7 +4,7 @@ default['splunk']['dest-port']   = '9997'
 
 # inputs.conf configuration
 # Format : [ [ "<PATH>" , "<SOURCETYPE>", "<INDEX>" ]
-default['splunk']['inputs']		= [ [ 'c:\logs\PF.PlatformServices\*.log' , 'platformservices', 'pfaws' ] , [ 'c:\logs\PF.Listeners\*.log' , 'pflisteners' , 'pfaws' ] ]
+default['splunk']['inputs']		= []
 
 # URL location of splunk installer
 default['splunk']['url']		 = 'http://s3.amazonaws.com/pfchefdev/binaries/splunkforwarder.rpm'
