6939457262e7f7da0be167ad644db8f066af3c20
hparmar <hparmar@e488b3dd-d2c1-224d-8c88-7181da6ea77a>
2013-02-26 21:24:36 +0000

    Add cake-base recipe.
    git-svn-id: https://pf-subversion.hq.practicefusion.com/svn/pf.build/trunk/chef-repo@2246 e488b3dd-d2c1-224d-8c88-7181da6ea77a

---
+++ cookbooks/cake-base/recipes/default.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: cake-base
+# Recipe:: default
+#
+# Copyright 2013, Practice Fusion
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Packages for cake
+# * Rmagick
+# * tesseract
+# * freetds
+# * curl-devel
+
+packages = %w'curl-devel ImageMagick ImageMagick-devel freetds freetds-devel'
+
+packages.each do |pkg|
+  package pkg do
+    action :upgrade
+  end
+end
+
+# Folders for the Rails app
+directory "/var/www/cake" do
+  owner "cake"
+  group "cake"
+  mode 00755
+  action :create
+  recursive true
+end
+
+# Give /var/www/cake to the cake user
+script "execute" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  code <<-DOC
+  chown -R cake:cake /var/www/cake
+  DOC
+end
