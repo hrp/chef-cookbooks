1be35e266d9b020685981a3d9d289b7fa9a5cfd1
hparmar <hparmar@e488b3dd-d2c1-224d-8c88-7181da6ea77a>
2013-02-26 23:07:40 +0000

    Version bump.
    git-svn-id: https://pf-subversion.hq.practicefusion.com/svn/pf.build/trunk/chef-repo@2255 e488b3dd-d2c1-224d-8c88-7181da6ea77a

--- cookbooks/cake-unicorn/metadata.rb
+++ cookbooks/cake-unicorn/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'hparmar@practicefusion.com'
 license          'All Rights Reserved'
 description      'Installs/Configures cake-unicorn'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'cake-base'
